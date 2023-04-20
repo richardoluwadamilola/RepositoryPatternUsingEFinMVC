80d6b66f21266b76fd8db36ec0f020fed8fdf56e
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-04-20 11:39:40 +0100

    Add project files.

---
+++ RepositoryUsingEFinMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,88 @@
+﻿using RepositoryUsingEFinMVC.DAL;
+using RepositoryUsingEFinMVC.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepositoryUsingEFinMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public EmployeeController()
+        {
+            _employeeRepository = new EmployeeRepository(new EmployeeDBContext());
+        }
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var model = _employeeRepository.GetAll();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeRepository.Insert(model);
+                _employeeRepository.Save();
+                return RedirectToAction("Index", "Employee");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditEmployee(int EmployeeID)
+        {
+            Employee model = _employeeRepository.GetByID(EmployeeID);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditEmployee(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeRepository.Update(model);
+                _employeeRepository.Save();
+                return RedirectToAction("Index", "Employee");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteEmployee(int EmployeeID)
+        {
+            Employee model = _employeeRepository.GetByID(EmployeeID);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int EmployeeID)
+        {
+            _employeeRepository.Delete(EmployeeID);
+            _employeeRepository.Save();
+            return RedirectToAction("Index", "Employee");
+        }
+    }
+
+}
---
+++ RepositoryUsingEFinMVC/Repository/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using RepositoryUsingEFinMVC.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryUsingEFinMVC.Repository
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetByID(int EmployeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+
+    }
+}
